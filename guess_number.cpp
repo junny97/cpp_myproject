b4c98d72091328a9129557c55f617fb8a42e2483
Jeff Yoon <jeffyoon@users.noreply.github.com>
2021-09-17 23:42:03 +0900

    Add comments for srand and rand

--- guess_number.cpp
+++ guess_number.cpp
@@ -1,15 +1,19 @@
 #include <iostream>
-#include <stdlib.h> // srand()
+#include <stdlib.h> // srand() and rand() are located in <stdlib.h>, so you need to include this header to use random functions
 
 
 using namespace std;
 
 int main()
 {
-  int my_num, my_rand;
+    int my_num, my_rand;
     int cnt=0;; // 몇번 예측했는지 설정
+  
+    // srand(x) function sets the seed of the random number generator algorithm used by the function rand()
+    // so, we use seed that is used in srand() as the time(NULL) - current time 
     srand((unsigned)time(NULL));
 
+    // rand() generates a number between 0 ~ 32367
     my_rand=rand()%100+1;
 
     while(1)
@@ -34,15 +38,14 @@ int main()
         cnt++; // 한 번 시도할떄마다 예측 카운트 1씩 올라감
     }
 
-         if(my_num==0) 
-         {
+    if (my_num == 0) 
+    {
          cout<<"포기하셨군요. 다음 기회에"<<endl;
-         }
-
-        else 
-        {
+    }
+    else 
+    {
         cout<<cnt<<"번째만에 정답을 맞추셨습니다."<<endl; 
-        }
+    }
     
-return 0;
+    return 0;
 }
